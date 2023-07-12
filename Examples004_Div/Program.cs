afd9496f9f0b78ed4fd567e490da61c924c915b9
DALER DADADZHANOVe <d.dadadzhanov87@gmail.com>
2023-07-12 15:11:50 +0600

    Div

---
+++ Examples004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿// int numberA = 10;
+// int nuumberB = 5;
+// Console.WriteLine(numberA / nuumberB);
+
+// // double numberA = 12;
+// double nuumberB = 5;
+// Console.WriteLine(numberA / nuumberB);
